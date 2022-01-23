6a710f6151bae65eff67d6ad47ff4cf8685ee9be
Jens Dumortier <72012366+DumortierJens@users.noreply.github.com>
2022-01-23 15:43:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const SPOTIFY_API_ID = 'bab85d57409248888588bc533bfb7a6f'; // your api id
-const SPOTIFY_API_SECRET = '4887c0c8c90743ce8e74fca5e8433210'; // your api secret
-const SPOTIFY_API_REDIRECT = 'http://topify.jensdumortier.be'; // your api redirect
+const SPOTIFY_API_ID = ''; // your api id
+const SPOTIFY_API_SECRET = ''; // your api secret
+const SPOTIFY_API_REDIRECT = ''; // your api redirect
